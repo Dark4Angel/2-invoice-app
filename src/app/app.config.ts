5fdf45d72191540437946b62fd3eda99776ce48c
Dark4Angel <quakextreme@msn.com>
2024-05-13 03:37:41 -0500

    app invoice

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { InvoiceService } from './services/invoice.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), InvoiceService]
 };
---
+++ src/app/components/list-items/list-items.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Item } from '../../models/item';
+import { RowItemComponent } from '../row-item/row-item.component';
+
+@Component({
+  selector: 'list-items',
+  standalone: true,
+  imports: [RowItemComponent],
+  templateUrl: './list-items.component.html',
+  styleUrl: './list-items.component.css'
+})
+export class ListItemsComponent {
+
+  @Input() items: Item[] = []
+
+  @Output() removeEventEmitter: EventEmitter<number> = new EventEmitter();
+
+  onRemove(id: number) {
+    this.removeEventEmitter.emit(id);
+  }
+
+}
